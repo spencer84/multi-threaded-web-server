de61368f8efe28b36c4cf07c6b8887fb9cf830b1
Sam Spencer <sam95@vt.edu>
2024-09-28 14:58:48 +0100

    Quick HTML response to request

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -21,7 +22,13 @@ fn handle_connection(mut stream: TcpStream){
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
